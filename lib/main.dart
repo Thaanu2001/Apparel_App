871abb84982381a96e1518ec3799a379cd15de98
Thaanu Perera <thaanup@gmail.com>
2021-04-11 03:50:37 +0530

    Side drawer UI - 1

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:Apparel_App/services/sidebaricons_icons.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -24,9 +25,132 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  var scaffoldKey = GlobalKey<ScaffoldState>();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      key: scaffoldKey,
+      drawer: Theme(
+        //* Side drawer ---------------------------------------------------------------------------------------------
+        data: Theme.of(context).copyWith(
+            // Set the transparency here
+            canvasColor: Colors.grey[300].withOpacity(0.9)),
+        child: Container(
+          width: 250,
+          child: Drawer(
+            child: Column(
+              // Important: Remove any padding from the ListView.
+              mainAxisSize: MainAxisSize.max,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Container(
+                  height: 100,
+                  width: double.infinity,
+                  child: DrawerHeader(
+                    child: Text(
+                      //* Drawer header
+                      'Gadget Doctor',
+                      style: TextStyle(
+                          fontFamily: 'sf',
+                          fontSize: 22,
+                          color: Colors.white,
+                          fontWeight: FontWeight.w600),
+                    ),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+                ListTile(
+                  contentPadding: EdgeInsets.only(left: 30),
+                  leading: Icon(
+                    Sidebaricons.bag,
+                    size: 22,
+                    color: Colors.black,
+                  ),
+                  title: Align(
+                    alignment: Alignment(-1.2, 0),
+                    child: Text(
+                      'My Orders',
+                      style: TextStyle(
+                          fontFamily: 'sf',
+                          fontSize: 20,
+                          color: Colors.black,
+                          fontWeight: FontWeight.w400),
+                    ),
+                  ),
+                  onTap: () {},
+                ),
+                ListTile(
+                  contentPadding: EdgeInsets.only(left: 30),
+                  leading: Icon(
+                    Icons.format_list_bulleted_rounded,
+                    color: Colors.black,
+                  ),
+                  title: Align(
+                    alignment: Alignment(-1.2, 0),
+                    child: Text(
+                      'Wish List',
+                      style: TextStyle(
+                          fontFamily: 'sf',
+                          fontSize: 20,
+                          color: Colors.black,
+                          fontWeight: FontWeight.w400),
+                    ),
+                  ),
+                  onTap: () {},
+                ),
+                ListTile(
+                  contentPadding: EdgeInsets.only(left: 32),
+                  leading: Icon(
+                    Sidebaricons.settings,
+                    size: 22,
+                    color: Colors.black,
+                  ),
+                  title: Align(
+                    alignment: Alignment(-1.2, 0),
+                    child: Text(
+                      'Settings',
+                      style: TextStyle(
+                          fontFamily: 'sf',
+                          fontSize: 20,
+                          color: Colors.black,
+                          fontWeight: FontWeight.w400),
+                    ),
+                  ),
+                  onTap: () {},
+                ),
+                Expanded(
+                  //* Sign Out button
+                  child: Align(
+                    alignment: Alignment.bottomCenter,
+                    child: ListTile(
+                      leading: Icon(
+                        Icons.logout,
+                        color: Colors.black,
+                      ),
+                      contentPadding: EdgeInsets.fromLTRB(30, 0, 0, 5),
+                      title: Transform.translate(
+                        offset: Offset(-15, 0),
+                        child: Text(
+                          'Sign Out',
+                          style: TextStyle(
+                              fontFamily: 'sf',
+                              fontSize: 20,
+                              color: Colors.black,
+                              fontWeight: FontWeight.w400),
+                        ),
+                      ),
+                      onTap: () {},
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
       backgroundColor: Color(0xffF3F3F3),
       body: Column(
         children: [
@@ -45,7 +169,7 @@ class _MyHomePageState extends State<MyHomePage> {
                       size: 32,
                       color: Color(0xff646464),
                     ),
-                    onTap: () {},
+                    onTap: () => scaffoldKey.currentState.openDrawer(),
                   ),
                 ),
                 SizedBox(
