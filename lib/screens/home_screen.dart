c1c49f7b96a0fd28da76fb386691a596785a0567
Thaanu Perera <thaanup@gmail.com>
2021-04-11 15:15:07 +0530

    Add tabbar UI

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,283 @@
+import 'package:flutter/material.dart';
+import 'package:Apparel_App/services/sidebaricons_icons.dart';
+import 'package:Apparel_App/widgets/scroll_glow_disabler.dart';
+
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  var scaffoldKey = GlobalKey<ScaffoldState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      resizeToAvoidBottomPadding: false,
+      key: scaffoldKey,
+      drawer: Theme(
+        //* Side drawer ---------------------------------------------------------------------------------------------
+        data: Theme.of(context).copyWith(
+            // Set the transparency here
+            canvasColor: Colors.grey[300].withOpacity(0.9)),
+        child: Container(
+          width: 250,
+          child: Drawer(
+            child: Column(
+              // Important: Remove any padding from the ListView.
+              mainAxisSize: MainAxisSize.max,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Container(
+                  height: 100,
+                  width: double.infinity,
+                  child: DrawerHeader(
+                    child: Text(
+                      //* Drawer header
+                      'Gadget Doctor',
+                      style: TextStyle(
+                          fontFamily: 'sf',
+                          fontSize: 22,
+                          color: Colors.white,
+                          fontWeight: FontWeight.w600),
+                    ),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+                ListTile(
+                  contentPadding: EdgeInsets.only(left: 30),
+                  leading: Icon(
+                    Sidebaricons.bag,
+                    size: 22,
+                    color: Colors.black,
+                  ),
+                  title: Align(
+                    alignment: Alignment(-1.2, 0),
+                    child: Text(
+                      'My Orders',
+                      style: TextStyle(
+                          fontFamily: 'sf',
+                          fontSize: 20,
+                          color: Colors.black,
+                          fontWeight: FontWeight.w400),
+                    ),
+                  ),
+                  onTap: () {},
+                ),
+                ListTile(
+                  contentPadding: EdgeInsets.only(left: 30),
+                  leading: Icon(
+                    Icons.format_list_bulleted_rounded,
+                    color: Colors.black,
+                  ),
+                  title: Align(
+                    alignment: Alignment(-1.2, 0),
+                    child: Text(
+                      'Wish List',
+                      style: TextStyle(
+                          fontFamily: 'sf',
+                          fontSize: 20,
+                          color: Colors.black,
+                          fontWeight: FontWeight.w400),
+                    ),
+                  ),
+                  onTap: () {},
+                ),
+                ListTile(
+                  contentPadding: EdgeInsets.only(left: 32),
+                  leading: Icon(
+                    Sidebaricons.settings,
+                    size: 22,
+                    color: Colors.black,
+                  ),
+                  title: Align(
+                    alignment: Alignment(-1.2, 0),
+                    child: Text(
+                      'Settings',
+                      style: TextStyle(
+                          fontFamily: 'sf',
+                          fontSize: 20,
+                          color: Colors.black,
+                          fontWeight: FontWeight.w400),
+                    ),
+                  ),
+                  onTap: () {},
+                ),
+                Expanded(
+                  //* Sign Out button
+                  child: Align(
+                    alignment: Alignment.bottomCenter,
+                    child: ListTile(
+                      leading: Icon(
+                        Icons.logout,
+                        color: Colors.black,
+                      ),
+                      contentPadding: EdgeInsets.fromLTRB(30, 0, 0, 5),
+                      title: Transform.translate(
+                        offset: Offset(-15, 0),
+                        child: Text(
+                          'Sign Out',
+                          style: TextStyle(
+                              fontFamily: 'sf',
+                              fontSize: 20,
+                              color: Colors.black,
+                              fontWeight: FontWeight.w400),
+                        ),
+                      ),
+                      onTap: () {},
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+      backgroundColor: Color(0xffF3F3F3),
+      body: Column(
+        children: [
+          Container(
+            padding: EdgeInsets.fromLTRB(20, 50, 20, 0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Flexible(
+                  //* Side menu icon ---------------------------------------------------------------------------
+                  flex: 1,
+                  fit: FlexFit.tight,
+                  child: InkWell(
+                    child: Icon(
+                      Icons.menu_rounded,
+                      size: 32,
+                      color: Color(0xff646464),
+                    ),
+                    onTap: () => scaffoldKey.currentState.openDrawer(),
+                  ),
+                ),
+                SizedBox(
+                  width: 15,
+                ),
+                Flexible(
+                  //* Search bar --------------------------------------------------------------------------------
+                  flex: 10,
+                  fit: FlexFit.tight,
+                  child: Container(
+                    height: 40,
+                    // color: Colors.red,
+                    child: TextField(
+                      cursorColor: Color(0xff646464),
+                      style: TextStyle(
+                          fontFamily: 'sf',
+                          fontSize: 18,
+                          color: Color(0xff646464),
+                          fontWeight: FontWeight.w500),
+                      decoration: new InputDecoration(
+                        isDense: true,
+                        contentPadding:
+                            EdgeInsets.symmetric(horizontal: 14, vertical: 0),
+                        suffixIcon: Icon(
+                          Icons.search,
+                          size: 28,
+                          color: Color(0xff646464),
+                        ),
+                        filled: true,
+                        fillColor: Colors.white,
+                        focusColor: Colors.red,
+                        enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(12.0)),
+                          borderSide: BorderSide(color: Colors.white, width: 0),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(12.0)),
+                          borderSide: BorderSide(color: Colors.white, width: 0),
+                        ),
+                      ),
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ),
+          SizedBox(
+            height: 6,
+          ),
+          ScrollGlowDisabler(
+            //* Top Tab Bar ------------------------------------------------------------------------------
+            child: DefaultTabController(
+              length: 4, // length of tabs
+              initialIndex: 0,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  Container(
+                    child: TabBar(
+                      //* Tab bar customize
+                      isScrollable: false,
+                      indicatorColor: Colors.transparent,
+                      labelColor: Colors.black,
+                      unselectedLabelColor: Color(0xffA4A4A4),
+                      labelStyle: TextStyle(
+                          fontFamily: 'sf',
+                          fontSize: 16,
+                          fontWeight: FontWeight.w600),
+                      unselectedLabelStyle: TextStyle(
+                          fontFamily: 'sf',
+                          fontSize: 16,
+                          fontWeight: FontWeight.w500),
+                      tabs: [
+                        Tab(text: 'Women'),
+                        Tab(text: 'Men'),
+                        Tab(text: 'Kids'),
+                        Tab(text: 'Stores'),
+                      ],
+                    ),
+                  ),
+                  Container(
+                    //* Tab Content
+                    height: 400,
+                    child: TabBarView(
+                      physics: NeverScrollableScrollPhysics(),
+                      children: <Widget>[
+                        Container(
+                          child: Center(
+                            child: Text('Display Tab 1',
+                                style: TextStyle(
+                                    fontSize: 22, fontWeight: FontWeight.bold)),
+                          ),
+                        ),
+                        Container(
+                          child: Center(
+                            child: Text('Display Tab 2',
+                                style: TextStyle(
+                                    fontSize: 22, fontWeight: FontWeight.bold)),
+                          ),
+                        ),
+                        Container(
+                          child: Center(
+                            child: Text('Display Tab 3',
+                                style: TextStyle(
+                                    fontSize: 22, fontWeight: FontWeight.bold)),
+                          ),
+                        ),
+                        Container(
+                          child: Center(
+                            child: Text('Display Tab 4',
+                                style: TextStyle(
+                                    fontSize: 22, fontWeight: FontWeight.bold)),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
